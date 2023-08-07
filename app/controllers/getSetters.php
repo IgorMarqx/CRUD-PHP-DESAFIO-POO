82f54d753a755b91b5cf41adcc1f6fea852ca472
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-07 08:25:45 -0300

    feat: class get and setters

---
+++ app/controllers/getSetters.php
@@ -0,0 +1,60 @@
+<?php
+
+class getSetters
+{
+    private $name;
+    private $email;
+    private $cpf;
+    private $telephone;
+    private $birthdate;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->name = $email;
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf($cpf)
+    {
+        $this->$cpf = $cpf;
+    }
+
+    public function getTelephone()
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone($telephone)
+    {
+        $this->telephone = $telephone;
+    }
+
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate($birthdate)
+    {  
+        $this->birthdate = $birthdate;
+    }
+}
