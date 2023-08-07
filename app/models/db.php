d15e8492fc706131750e70a7df2f79ec2abeb262
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-07 09:38:00 -0300

    return the connect pdo

--- app/models/db.php
+++ app/models/db.php
@@ -3,8 +3,10 @@ $usename = 'root';
 $password = '';
 
 try{
-    $pdo = new PDO('mysql:dbname=desafiophp;host=localhost', $username, $password);
+    $pdo = new PDO('mysql:dbname=desafiophp;host=localhost', $usename, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $pdo;
 }catch(PDOException $e){
     echo 'ERROR: ' . $e->getMessage();
 }
