f5813563e85d78ab3d43f3503baba10b95fb0f7a
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-07 09:36:51 -0300

    create form teste

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="./app/controllers/createController.php" method="POST">
+    <input type="text" name="name">
+    <input type="text" name="email">
+    <input type="text" name="cpf">
+    <input type="text" name="telephone">
+    <input type="text" name="birthdate">
+
+    <input type="submit" value="enviar">
+</form>
