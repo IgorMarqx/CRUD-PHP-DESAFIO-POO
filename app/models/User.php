3deffeb476056728736b388599fe0aa7c428bf5c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-08-08 09:28:14 -0300

    feat: create repository for register on bd

---
+++ app/models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+class User
+{
+    private $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        return $this->pdo = $pdo;
+    }
+
+    public function insert($name, $email, $cpf, $telephone, $birthdate)
+    {
+        $query = "INSERT INTO clientes (name, email, cpf, telephone,birthdate) VALUES (?,?,?,?,?)";
+        $insert = $this->pdo->prepare($query);
+        $insert->bindValue(1, $name);
+        $insert->bindValue(2, $email);
+        $insert->bindValue(3, $cpf);
+        $insert->bindValue(4, $telephone);
+        $insert->bindValue(5, $birthdate);
+
+        try {
+            $insert->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
